b821fae5ce7259c2e5b40586dfc52a60db82de33
severinsimmler <severin.simmler@posteo.de>
2018-08-23 19:00:02 +0200

    chore: finalize corpus

--- src/cophi/api.py
+++ src/cophi/api.py
@@ -14,15 +14,15 @@ import cophi.model
 
 
 def document(filepath, **kwargs):
-    """Read text file and create a Document object.
+    """Read a text file and create a Document object.
 
     Parameter:
         filepath (str): Path to the text file.
         title (str): Text file’s title (optional).
-        lowercase (bool): If True, lowercase all letters (optional).
+        lowercase (bool): If True, all letters are lowercase (optional).
         ngrams (int): Number of tokens per ngram (optional).
-        pattern (str): Regex pattern for one token (optional).
-        maximum (int): If not None, stop reading after that much tokens (optional).
+        token_pattern (str): Regex pattern for one token (optional).
+        maximum (int): Stop tokenizing after that much tokens (optional).
 
     Returns:
         A Document object.
@@ -30,39 +30,44 @@ def document(filepath, **kwargs):
     textfile = model.Textfile(filepath)
     return cophi.model.Document(textfile.content, **kwargs)
 
-def pipe(directory: Union[str, pathlib.Path], pathname_pattern: str = "*.*",
-         treat_as: Optional[str] = None, encoding: str = "utf-8", lowercase: bool = True,
-         ngrams: int = 1, token_pattern: str = r"\p{L}+\p{P}?\p{L}+",
-         maximum: Optional[int] = None) -> Tuple[model.Corpus, pd.DataFrame]:
-    """Pipe a collection of text files through multiple NLP tasks.
+
+def corpus(directory, filepath_pattern="*.*", treat_as=None, encoding="utf-8",
+           lowercase=True, ngrams=1, token_pattern=r"\p{L}+\p{P}?\p{L}+",
+           maximum=None):
+    """Pipe a collection of text files and create a Corpus object.
 
     Parameters:
-        directory: Path to the corpus directory.
-        pathname_pattern: Glob pattern for text files.
-        treat_as: Treat text files like this suffix. If None, `pathname_pattern` is considered.
-        encoding: Encoding to use for UTF when reading.
-        lowercase: If True, all letters are lowercase.
-        ngrams: The `n` in ngram, e.g. 1 for unigram, 2 for bigram, etc.
-        token_pattern: Regex pattern for a token.
-        maximum: If not None, stop tokenizing after that much tokens.
+        directory (str): Path to the corpus directory.
+        filepath_pattern (str): Glob pattern for text files (optional).
+        treat_as (str): Treat text files like this suffix (optional).
+        encoding (str): Encoding to use for UTF when reading (optional).
+        lowercase (bool): If True, all letters are lowercase (optional).
+        ngrams (int): Number of tokens per ngram (optional).
+        token_pattern (str): Regex pattern for one token (optional).
+        maximum (int): Stop tokenizing after that much tokens (optional).
 
     Returns:
-        A Corpus model object and a pandas DataFrame with metadata.
+        A Corpus model object and a Metadata object.
     """
     if isinstance(directory, str):
         directory = pathlib.Path(directory)
-    filepaths = directory.glob(pathname_pattern)
+    filepaths = directory.glob(filepath_pattern)
 
     def lazy_reading(filepaths):
         for filepath in filepaths:
-            yield model.Textfile(filepath, treat_as, encoding)
+            yield cophi.model.Textfile(filepath, treat_as, encoding)
 
-    metadata = pd.DataFrame()
+    metadata = cophi.model.Metadata()
     documents = pd.Series()
     for textfile in lazy_reading(filepaths):
         document_id = str(uuid.uuid1())
         text = textfile.content
-        document = model.Document(text, document_id, lowercase, ngrams, token_pattern, maximum)
+        document = cophi.model.Document(text,
+                                        document_id,
+                                        lowercase,
+                                        ngrams,
+                                        token_pattern,
+                                        maximum)
         documents[document_id] = document
         metadata = metadata.append({"uuid": document_id,
                                     "filepath": textfile.filepath,
@@ -70,4 +75,4 @@ def pipe(directory: Union[str, pathlib.Path], pathname_pattern: str = "*.*",
                                     "title": textfile.title,
                                     "suffix": textfile.filepath.suffix},
                                     ignore_index=True)
-    return model.Corpus(documents), metadata
+    return cophi.model.Corpus(documents), metadata
